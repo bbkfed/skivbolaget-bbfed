e83554cd02cc2107b40ab769119dcf185c5482aa
bbkfed <balthazarberling+fed@gmail.com>
2020-03-31 16:36:02 +0200

    h

--- src/routes/albumRoute.js
+++ src/routes/albumRoute.js
@@ -7,7 +7,6 @@ const Album = require("../../model/album");
 app.get(ROUTE.album, async (req, res) => {
 
     const album = await Album.findOne( { _id: req.params.id });
-    console.log(album);
 
     res.render(VIEW.album, { album });
 });
